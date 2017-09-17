38526338d87010bb74e81fe0d820675eef15bbd9
Cinder Biscuits <cinder@cinderblocks.biz>
2017-09-17 11:20:27 -0500

    Switch to libressl as openssl available is too old.

--- conanfile.py
+++ conanfile.py
@@ -88,9 +88,9 @@ class QtConan(ConanFile):
     def requirements(self):
         if self.settings.os == "Windows":
             if self.options.openssl == "yes":
-                self.requires("OpenSSL/1.0.2l@conan/stable", dev=True)
+                self.requires("libressl/2.5.3@hoxnox/testing", dev=True)
             elif self.options.openssl == "linked":
-                self.requires("OpenSSL/1.0.2l@conan/stable")
+                self.requires("libressl/2.5.3@hoxnox/testing")
 
     def source(self):
         submodules = ["qtbase"]
